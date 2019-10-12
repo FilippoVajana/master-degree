b1ae18d91ff3af38128a0a4c7e9b34d8ed3407b7
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-10-12 19:40:47 +0200

    add save/load config file in engine class

--- engine/__init__.py
+++ engine/__init__.py
@@ -2,16 +2,16 @@ import logging
 import time
 from tqdm import tqdm
 
-from engine import trainer, tester
+# from engine import trainer, tester
 from engine.logger import Logger
 
 # create logger
 logger = logging.getLogger(name="engine.logger")
-logger.setLevel(lvl=logging.INFO)
+logger.setLevel(logging.INFO)
 # create console handler
 ch = logging.StreamHandler()
-ch.setLevel(lvl=logging.INFO)
+ch.setLevel(logging.INFO)
 # create formatter
 formatter = logging.Formatter('[%(asctime)s]  %(levelname)s: %(message)s', datefmt='%I:%M:%S')
-ch.setFormatter(form=formatter)
-logger.addHandler(hdlr=ch)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
