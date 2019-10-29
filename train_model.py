4006f335defbdb11ad75aa3b863c7b223017cab7
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-10-29 22:39:15 +0100

    add train script

---
+++ train_model.py
@@ -0,0 +1,22 @@
+import engine
+
+MODELS = {
+    'LeNet5': engine.LeNet5()
+}
+
+
+if __name__ == '__main__':
+    run_cfg_path = './runconfig.json'
+
+    # save reference RunConfig json
+    # engine.RunConfig().save(run_cfg_path)
+
+    # load reference RunConfig json
+    run_cfg = engine.RunConfig().load(run_cfg_path)
+
+    # get model
+    model = MODELS[run_cfg.model]
+    run_cfg.model = model # swaps model classname with model instance
+
+    # train model
+    model.start_training(run_cfg)
