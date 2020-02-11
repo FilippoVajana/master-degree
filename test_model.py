67f9b3e54920b09e8619ca178592d1feb12184dd
Filippo Vajana <filippo.vajana@orobix.com>
2020-02-11 11:04:34 +0100

    remove readme

--- test_model.py
+++ test_model.py
@@ -1,16 +1,12 @@
-import argparse
 import os
-
-import GPUtil
-import numpy as np
-import pandas as pd
+import argparse
 import torch
 import torchvision.transforms as transforms
-from skimage import transform
-from torch.cuda import is_available
-
+import pandas as pd
+import numpy as np
 import engine
 from engine.tester import Tester
+from skimage import transform
 
 MODELS = {
     'LeNet5': engine.LeNet5()
@@ -25,18 +21,6 @@ DATA_DICT = {
     'no-mnist': './data/no-mnist'
 }
 
-DEVICE = "cpu"
-
-
-def get_device():
-    device = "cpu"  # default device
-    # check cuda device availability
-    if is_available():
-        gpu = GPUtil.getFirstAvailable()  # get best GPU
-        device = f"cuda:{gpu[0]}"
-    print("Selected device: ", device)
-    return device
-
 
 def test_regular_data(model, dataset_name):
     # get dataloader
@@ -48,7 +32,7 @@ def test_regular_data(model, dataset_name):
     ).build(train_mode=False, max_items=-1, validation_ratio=0)
 
     # test model
-    tester = Tester(model, device=DEVICE, is_ood=False)
+    tester = Tester(model, device="cuda", is_ood=False)
     df = tester.test(dataloader[0])
     return df
 
@@ -63,7 +47,7 @@ def test_ood_data(model, dataset_name):
     ).build(train_mode=False, max_items=-1, validation_ratio=0)
 
     # test model
-    tester = Tester(model, device=DEVICE, is_ood=True)
+    tester = Tester(model, device="cuda", is_ood=True)
     df = tester.test(dataloader[0])
     return df
 
@@ -84,7 +68,7 @@ def test_rotated_data(model, dataset_name, rotation_value=45):
     ).build(train_mode=False, max_items=-1, validation_ratio=.0)
 
     # test model
-    tester = Tester(model, device=DEVICE, is_ood=False)
+    tester = Tester(model, device="cuda", is_ood=False)
     df = tester.test(dataloader[0])
     return df
 
@@ -105,7 +89,7 @@ def test_shifted_data(model, dataset_name, shift_value=45):
     ).build(train_mode=False, max_items=-1, validation_ratio=.0)
 
     # test model
-    tester = Tester(model, device=DEVICE, is_ood=False)
+    tester = Tester(model, device="cuda", is_ood=False)
     df = tester.test(dataloader[0])
     return df
 
@@ -120,32 +104,20 @@ if __name__ == '__main__':
                         help='Test data directory.')
     args = parser.parse_args()
 
-    # get compute device
-    DEVICE = get_device()
-
     # load model
     model = MODELS[args.n]
-    if DEVICE is "cpu":
-        model.load_state_dict(torch.load(
-            args.m, map_location=torch.device('cpu')))
-    else:
-        model.load_state_dict(torch.load(args.m))
+    model.load_state_dict(torch.load(args.m, map_location=torch.device('cpu')))
 
     # prepare test folder
     df_path = os.path.join(RUNS_DICT['LeNet5'], "test")
     os.makedirs(df_path, exist_ok=True)
-
-    # TEST LOOP
-    test_dataframes = list()  # (dataframe : save path)
 
     # test In-Distribution
     if True:
         try:
             print("Testing MNIST")
             mnist_df = test_regular_data(model, "mnist")
-            #mnist_df.to_csv(df_path + os.sep + "mnist.csv", index=True)
-            test_dataframes.append(
-                (mnist_df, os.path.join(df_path, "mnist.csv")))
+            mnist_df.to_csv(df_path + os.sep + "mnist.csv", index=True)
         except Exception as exc:
             print(exc)
 
@@ -156,9 +128,8 @@ if __name__ == '__main__':
             try:
                 print(f"Testing Rotated {rotation_value} MNIST")
                 rotated_df = test_rotated_data(model, "mnist", rotation_value)
-                #rotated_df.to_csv(df_path + os.sep + f"mnist_rotate{rotation_value}.csv", index=True)
-                test_dataframes.append((rotated_df, os.path.join(
-                    df_path, f"mnist_rotate{rotation_value}.csv")))
+                rotated_df.to_csv(df_path + os.sep +
+                                  f"mnist_rotate{rotation_value}.csv", index=True)
             except Exception as exc:
                 print(exc)
 
@@ -170,10 +141,9 @@ if __name__ == '__main__':
             shift_value /= img_size
             try:
                 print(f"Testing Shifted {int(shift_value * img_size)}px MNIST")
-                shifted_df = test_shifted_data(model, "mnist", shift_value)
-                # shifted_df.to_csv(df_path + os.sep + f"mnist_shift{int(shift_value * img_size)}.csv", index=True)
-                test_dataframes.append((shifted_df, os.path.join(
-                    df_path, f"mnist_shift{int(shift_value * img_size)}.csv")))
+                rotated_df = test_shifted_data(model, "mnist", shift_value)
+                rotated_df.to_csv(df_path + os.sep +
+                                  f"mnist_shift{int(shift_value * img_size)}.csv", index=True)
             except Exception as exc:
                 print(exc)
 
@@ -182,14 +152,6 @@ if __name__ == '__main__':
         try:
             print("Testing OOD")
             ood_df = test_ood_data(model, "no-mnist")
-            #ood_df.to_csv(df_path + os.sep + "nomnist.csv", index=True)
-            test_dataframes.append(
-                (ood_df, os.path.join(df_path, "nomnist.csv")))
+            ood_df.to_csv(df_path + os.sep + f"nomnist.csv", index=True)
         except Exception as exc:
             print(exc)
-
-    # save results
-    if True:
-        for (df, path) in test_dataframes:
-            print("Saving to ", path)
-            df.to_csv(path, index=True)
