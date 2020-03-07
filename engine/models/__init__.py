36d22ca85e2ed56864f1b8c37168ee6cfae24963
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2020-03-07 17:29:31 +0100

    add LeNet5 model with an extra output class

--- engine/models/__init__.py
+++ engine/models/__init__.py
@@ -2,3 +2,4 @@ from engine.models.lenet5 import *
 from engine.models.lenet5simplelldropout import *
 from engine.models.lenet5simpledropout import *
 from engine.models.lenet5concretedropout import *
+from engine.models.lenet5labeldrop import *
