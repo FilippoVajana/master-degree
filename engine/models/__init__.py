e2e88b7f0d09bcc4ea29603b1c28a0bfe9016594
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2020-03-12 22:44:04 +0100

    add lenet5 model for transfer learning

--- engine/models/__init__.py
+++ engine/models/__init__.py
@@ -3,3 +3,4 @@ from engine.models.lenet5simplelldropout import *
 from engine.models.lenet5simpledropout import *
 from engine.models.lenet5concretedropout import *
 from engine.models.lenet5labeldrop import *
+from engine.models.lenet5transferlearning import *
